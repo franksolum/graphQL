fa1847c3d7e80b1aa817dcf0683ae054b12388be
franksolum <franksolum@gmail.com>
2024-05-30 16:03:31 +0100

    Update db, typeDefs, resolvers, index

--- index.js
+++ index.js
@@ -2,17 +2,37 @@
 import { ApolloServer } from "@apollo/server";  /* this is to set up our server,configure it and 
 tell apollos how to handle all our diifferent type of data and responses to query*/
 import { startStandaloneServer } from "@apollo/server/standalone"; /* to start up the server so that we can start listen to req*/
+//db
+import db from "./_db.js";
+//types
+import { typeDefs } from "./schema.js";
+
+const resolvers ={
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
 
 //server setup
 
 const server = new ApolloServer({
-    //typedefs
-    //resolvers
+    //typedefs == definitions of types of data eg author with different fields that users can eventually query
+    //resolvers --- resolver fn which handle incoming request/querry
+    typeDefs,
+    resolvers
 
 })
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000}
+    listen: {port: 4000}
 })
 
 console.log('serveer ready at port', 4000)
---
+++ schema.js
@@ -0,0 +1,30 @@
+export const typeDefs = `#graphql
+type Game {
+    id: ID!
+    title: String!
+    platform: [String]
+}
+type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+
+}
+type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+}
+type Query {
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+
+}
+
+
+
+
+`
+
+/*5 basic scaler type- int, string, boolean, ID(key to date obj), float */
